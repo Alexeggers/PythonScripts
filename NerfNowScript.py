dca136954e765e656845d77e1dfb51432df7beb2
Alex.Eggers <alex.eggers@xailabs.de>
2017-06-19 15:26:56 +0200

    Removed redundant code

--- NerfNowScript.py
+++ NerfNowScript.py
@@ -11,14 +11,13 @@ nerf_now_url = "http://www.nerfnow.com/comic/"
 start = 10
 
 # Upper border of comics, this one isn't downloaded
-finish = 11
+finish = 12
 
 # Time in seconds
 delay = 5
 
 def make_soup(url):
-    url_request = Request(url, headers = {"User-Agent": "Mozilla/5.0"})
-    html = urlopen(url_request).read()
+    html = urlopen(url).read()
     return BeautifulSoup(html, "html.parser")
 
 def get_image(number):
@@ -26,20 +25,20 @@ def get_image(number):
     found_div = soup.find('div', attrs = {'id': 'comic'})
     found_div = found_div.find('img')
     image_url = found_div['src']
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-    urllib.request.install_opener(opener)
     urlretrieve(image_url, number + ".jpg")
 
-def prep_directory():
+def prep_work():
     dir_name = "NerfNowComics"
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
     os.chdir(dir_name)
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
 
 if __name__ == "__main__":
     comic_numbers = list(range(start, finish))
-    prep_directory()
+    prep_work()
     for number in comic_numbers:
         get_image(str(number))
         sleep(delay)
