d35fd4f7053b4b695c74f7a56082168e70813965
Alex.Eggers <alex.eggers@xailabs.de>
2017-06-20 10:23:33 +0200

    Changed request module used
    From urllib2 to requests

--- NerfNowScript.py
+++ NerfNowScript.py
@@ -1,11 +1,12 @@
 import os
 import urllib
 
+from requests import get
 from time import sleep
 from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen, urlretrieve
 
 nerf_now_url = "http://www.nerfnow.com/comic/"
+dir_name = "NerfNowComics"
 
 # First comic to be downloaded. Do not go lower than 4,
 # it is the first for some reason.
@@ -18,7 +19,7 @@ finish = 4
 delay = 5
 
 def make_soup(url):
-    html = urlopen(url).read()
+    html = get(url).text
     return BeautifulSoup(html, "html.parser")
 
 def get_image(number):
@@ -26,16 +27,15 @@ def get_image(number):
     found_div = soup.find('div', attrs = {'id': 'comic'})
     image_element = found_div.find('img')
     image_url = image_element['src']
-    urlretrieve(image_url, number + ".jpg")
+    image_content = get(image_url).content
+    with open(number + ".jpg", "wb") as handler:
+        handler.write(image_content)
+    print(image_url)
 
 def prep_work():
-    dir_name = "NerfNowComics"
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
     os.chdir(dir_name)
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-    urllib.request.install_opener(opener)
 
 if __name__ == "__main__":
     comic_numbers = list(range(start, finish + 1))
